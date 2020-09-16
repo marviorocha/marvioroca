42fb57261ede77a0b4e0837e5b78bb2ce60655b9
marviorocha <marviorocha@marviorocha.com>
2020-09-15 23:53:01 -0300

    Updates

---
+++ precache-manifest.9f702cd58b7e825f4d5c906d3e51c3ed.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63d86d243b6c67d8a1a22ddc64216f43",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fff0040591a8d6292b55",
+    "url": "/static/css/main.4a4ff6e6.chunk.css"
+  },
+  {
+    "revision": "72299d23ca134cd4bd9a",
+    "url": "/static/js/2.db83fca7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.db83fca7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fff0040591a8d6292b55",
+    "url": "/static/js/main.a591a383.chunk.js"
+  },
+  {
+    "revision": "6a0c3c4eebbf9146d385",
+    "url": "/static/js/runtime-main.be82d964.js"
+  },
+  {
+    "revision": "b6a2237e45b50ff5319fcb0aeb21c013",
+    "url": "/static/media/footer-bg.b6a2237e.jpg"
+  },
+  {
+    "revision": "1deb58db63c826cc0fb427fe972baf99",
+    "url": "/static/media/header-bg.1deb58db.jpg"
+  },
+  {
+    "revision": "d3a494463e317fd426759917bcdec29e",
+    "url": "/static/media/logo.d3a49446.png"
+  },
+  {
+    "revision": "5b883c88d0c8c20de59fa8c4e5172664",
+    "url": "/static/media/oficial-foto.5b883c88.png"
+  },
+  {
+    "revision": "b32d0e630e22d5253f55c27f58d5e874",
+    "url": "/static/media/progressive_app.b32d0e63.jpg"
+  }
+]);
